d0c5685c4373f1a6c13c80858bc7b795e7cde32f
dalenti <114300375+dalenti@users.noreply.github.com>
2022-10-17 21:45:55 +0700

    Add files via upload

---
+++ SCTV.java
@@ -0,0 +1,13 @@
+
+    public class SCTV extends Televisi{
+    
+    public void ciriKhas(){
+        System.out.println("Satu Untuk Semua");
+    }
+
+    public static void main(String[] args) {
+        SCTV a = new SCTV();
+
+        a.ciriKhas();
+    }
+}
